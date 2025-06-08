dae8bd0196254a5d8e21b7bedc2bc87ef6aa6222
Stefano Lottini <stefano.lottini@datastax.com>
2025-06-09 01:54:39 +0200

    minor cleanup

--- automation_scripts/ec2_provision.sh
+++ automation_scripts/ec2_provision.sh
@@ -10,6 +10,4 @@ git clone https://github.com/hemidactylus/data-api-nb-test.git
 curl -L -O https://github.com/nosqlbench/nosqlbench/releases/latest/download/nb5
 chmod +x nb5
 
-./nb5 --version
-
 touch EC2_PROVISION_COMPLETE
