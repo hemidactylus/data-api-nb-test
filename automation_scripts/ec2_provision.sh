b7dcdd0e91f793448f56c3cd314e6b671d940def
Stefano Lottini <stefano.lottini@datastax.com>
2025-06-25 00:02:13 +0200

    pin nb version due to the symlink problem; re-enable ec2 deletion after perf testing

--- automation_scripts/ec2_provision.sh
+++ automation_scripts/ec2_provision.sh
@@ -7,7 +7,9 @@ sudo apt install -y git fuse3 rsync
 
 git clone https://github.com/hemidactylus/data-api-nb-test.git
 
-curl -L -O https://github.com/nosqlbench/nosqlbench/releases/latest/download/nb5
+# TODO: reinstate once the symlink issue with 5.21.8-release is fixed:
+# https://github.com/nosqlbench/nosqlbench/releases/latest/download/nb5
+curl -L -O https://github.com/nosqlbench/nosqlbench/releases/download/5.21.7-release/nb5
 chmod +x nb5
 
 touch EC2_PROVISION_COMPLETE
